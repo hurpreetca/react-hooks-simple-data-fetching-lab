bf4862a58f0f481dafb90699bbb0124935d97754
Harpreet Singh <hurpreet@icloud.com>
2023-06-10 14:36:18 -0400

    Lab Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [dogImg, setDogImg] = useState(null);
+
+  useEffect(() => {
+    const dogFetch = () =>
+      fetch("https://dog.ceo/api/breeds/image/random").then((res) =>
+        res.json()
+      );
+
+    async function dogInfoFetching() {
+      const dogData = await dogFetch();
+      setDogImg(dogData.message);
+      setLoading(false);
+    }
+    dogInfoFetching();
+  }, []);
+  if (loading) return <p>Loading...</p>;
+
+  return <img src={dogImg} alt="A Random Dog"></img>;
+}
+
+export default App;
